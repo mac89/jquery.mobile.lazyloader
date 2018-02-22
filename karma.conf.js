0914867a9cfbb4ad4ebb2ca6ca0122bfd497d57b
Maarten Manders <maarten.manders@dualinventive.com>
2018-02-22 17:14:43 +0100

    Changed code coverage to coveralls. Changed test script to 'grunt dev'.

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function( config ) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            "src/*.js": "coverage"
+            "**/src/*.js": "coverage"
         },
 
         // test results reporter to use
@@ -37,7 +37,7 @@ module.exports = function( config ) {
         reporters: [ "dots", "coverage", "junit" ],
 
         coverageReporter: {
-            type: "cobertura",
+            type: "lcov",
             dir: "coverage/"
         },
 
