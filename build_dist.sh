273534020922401501cad3226799001aee75ef61
Maarten Manders <maarten.manders@dualinventive.com>
2018-02-23 11:10:32 +0100

    Changed deploy-gh-pages.sh to build_dist.sh. This script is executed before deploy and commits the dist/ files.

---
+++ build_dist.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e #Set errors to fail explicitly
+
+# I only want to build and commit if I'm on master
+#if [ "${TRAVIS_BRANCH}" == "master" ]
+#then
+  # build dist
+  grunt dist
+
+  # initialize and commit everything in pages
+  cd dist
+  git init
+  git config user.name "Travis"
+  git config user.email "Travis"
+  git add .
+  git commit -m "build dist assets and commit to dist"
+
+  # hide my output for security reasons
+  # force it because we want this to just actually work
+  # note, I'm pushing to a remote branch for the dist assets from master
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" "${TRAVIS_BRANCH}":dist > /dev/null 2>&1
+#else
+#  echo "Current branch is not master and dist will not be built or committed."
+#fi
