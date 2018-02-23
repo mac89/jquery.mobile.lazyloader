7d41d0f5b485a39b1ccbbb96f6b9a54569a982e9
Maarten Manders <maarten.manders@dualinventive.com>
2018-02-23 11:29:35 +0100

    Possible fix for build script.

--- build_dist.sh
+++ build_dist.sh
@@ -13,12 +13,12 @@ set -e #Set errors to fail explicitly
   git config user.name "Travis"
   git config user.email "Travis"
   git add .
-  git commit -m "build dist assets and commit to dist"
+  git commit -m "build dist assets and commit to dist [ci skip]"
 
   # hide my output for security reasons
   # force it because we want this to just actually work
   # note, I'm pushing to a remote branch for the dist assets from master
-  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" "${TRAVIS_BRANCH}":dist > /dev/null 2>&1
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" $TRAVIS_BRANCH:dist > /dev/null 2>&1
 #else
 #  echo "Current branch is not master and dist will not be built or committed."
 #fi
