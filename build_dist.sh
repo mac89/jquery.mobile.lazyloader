95f4baa8d54f33b082b0804b8121fad5892640de
Maarten Manders <maarten.manders@dualinventive.com>
2018-02-23 12:07:11 +0100

    Added checkout for v2.0.0 to build_dist.sh

--- build_dist.sh
+++ build_dist.sh
@@ -10,6 +10,7 @@ set -e #Set errors to fail explicitly
   # initialize and commit everything in pages
   cd dist
   git init
+  git checkout v2.0.0
   git config user.name "Travis"
   git config user.email "Travis"
   git add .
@@ -18,7 +19,7 @@ set -e #Set errors to fail explicitly
   # hide my output for security reasons
   # force it because we want this to just actually work
   # note, I'm pushing to a remote branch for the dist assets from master
-  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}"  2>&1
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" v2.0.0:dist  2>&1
 #else
 #  echo "Current branch is not master and dist will not be built or committed."
 #fi
